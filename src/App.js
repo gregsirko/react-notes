5e02cbc903922b9d599ace92094db97624507c20
Greg <greg.sirko@icloud.com>
2026-03-13 01:27:14 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes ] = useState([]);
+  const [currentId, setCurrentId] = useState(null);
+
+  const handleAddNote = () => {
+    //id, title, content, lastmodified
+    const newNote = {
+      id: Date.now(),
+      title: "Untitled",
+      content: "",
+      lastModified: Date.now(),
+    };
+    setCurrentId(newNote.id);
+
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+      console.log(notes);
+  };
+
+  const handleSaveNote = () => {
+    console.log("");
+  };
+
+  const handleDeleteNote = () => {
+    console.log("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      <div class="notes-app">
+        <ul class="sidebar">
+          {notes.map((x) => (
+            <li key = {x.id}
+             class="sidebar-item">
+              {x.title}
+             </li>
+
+          ))}
+        </ul>
+        <main class="main-content">
+          <div className="button-row">
+            <button className="btn" onClick={handleAddNote}>
+              Add Note
+            </button>
+            <button className="btn" onClick={handleSaveNote}>
+              Save Note
+            </button>
+            <button className="btn" onClick={handleDeleteNote}>
+              Delete
+            </button>
+          </div>
+          <h1 key = {currentId}>{currentId}</h1>
+        </main>
+      </div>
     </div>
   );
 }
